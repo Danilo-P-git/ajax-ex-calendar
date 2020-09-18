2f061545d348810269d32118fd53133265bb43fd
Danilo Patanè <d.patane98@gmail.com>
2020-09-18 21:39:39 +0200

    WIP

--- js/script.js
+++ js/script.js
@@ -1,24 +1,12 @@
 $(document).ready(function(){
-
+var startYear = 2018;
+var startMonth = 0;
 var date = "2018-01-01";
 
 var momentDate = moment(date);
 
 var source = $("#day-template").html();
 var template = Handlebars.compile(source);
-
-for (var i = 1; i <= momentDate.daysInMonth(); i++) {
-  var day = addZero(i);
-  var dateComplete = momentDate.format("YYYY") + "-" + momentDate.format("MM") + "-" + day;
-  var context = {
-    "day": i,
-    "month": momentDate.format("MMMM"),
-    "dateComplete": dateComplete
-  };
-
-  var html = template(context);
-  $("#days").append(html);
-}
 
 
 $.ajax(
@@ -29,35 +17,45 @@ $.ajax(
       "month": 0
     },
     "method": "GET",
-    "success": function(data) {
-      printHolidays(data.response);
+    "success": function(data, status) {
+      printHolidays(data);
     },
     "error" : function(franco) {
       alert("Errore");
     }
   }
 );
-});
 
 
 
-function printHolidays(holidays) {
-  if (holidays.length > 0) {
-    for (var i = 0; i < holidays.length; i++) {
-      var holidayDate = holidays[i].date;
-      var holidayName = holidays[i].name;
+function printHolidays(data) {
+  var daysInMonth = momentDate.daysInMonth();
+  for (var i = 0; i <= daysInMonth; i++) {
+    var context =  {
+      "day": i,
+      "month": momentDate.format("MMMMM")
+    };
+  };
+  var html = $(template(context));
 
-      $(".day[data-date='"+ holidayDate +"']").addClass("holiday");
+  var currentMomentDate = moment(context.day + context.month + startYear);
+  var currentMomentDateFormat = currentMomentDate.format("YYYY-MM-DD");
 
-      $(".day[data-date='"+ holidayDate +"'] .holidayType").text("- " +holidayName);
+  if (data.response.lenght > 0) {
+    for (var j = 0; j < data.response.lenght; j++) {
+      if (data.response[j].date == currentMomentDateFormat) {
+        $(html).addClasse("holiday");
+
+        var holidayName = data.response[j].name;
+      $(html).append(" &mdash; <span>" + holidayName + "</span>");
+      }
     }
   }
+
+  $("#days")
 }
 
 
-function addZero(day) {
-  if (day < 10) {
-    return "0" + day
-  }
-  return day;
-}
+
+
+});
